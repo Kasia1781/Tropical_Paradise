6b6fcc1b3abcedf0e9d21770fc0297cfef4d5631
Kasia <kasia1781@gmail.com>
2022-09-05 20:24:52 +0200

    created site

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const nav = document.querySelector('.nav'); //przeszukujemy cały dokument żeby znaleźć nav
+const navBTN = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item');
+
+const handleNav = () => {
+    nav.classList.toggle('nav--active')
+
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('nav--active')
+        })
+    })
+    
+    handleNavItemsAnimation();
+}
+
+const handleNavItemsAnimation = () => {
+    let delayTime = 0;
+    
+    allNavItems.forEach(item => {
+        item.classList.toggle('nav-items-animation')
+        //animation-delay: .3s;
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
+}
+
+navBTN.addEventListener('click', handleNav)
